57f0ba284d1356671fb7d6bfd1bd260f5703615b
HamBP <sjy_9484@naver.com>
2022-04-27 20:59:20 +0900

    Initial Commit

---
+++ inu bus/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  inu bus
+//
+//  Created by 송준영 on 2022/04/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+            Text("Tab Content 1").tabItem { Text("인천대입구") }.tag(1)
+                Text("Tab Content 2").tabItem { Text("지식정보단지") }.tag(2)
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ inu bus/inu_busApp.swift
@@ -0,0 +1,17 @@
+//
+//  inu_busApp.swift
+//  inu bus
+//
+//  Created by 송준영 on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct inu_busApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
