5eeef1aae0c2fdd3e8bf89de48632b7db6b7f715
HamBP <sjy_9484@naver.com>
2022-04-28 12:46:50 +0900

    add : add components BusList & BusFeed

---
+++ inu bus/BusListView.swift
@@ -0,0 +1,25 @@
+//
+//  BusListView.swift
+//  inu bus
+//
+//  Created by 송준영 on 2022/04/28.
+//
+
+import SwiftUI
+
+struct BusListView: View {
+    var content: String = "test"
+    
+    var body: some View {
+        List {
+            Text("111")
+            Text("222")
+        }
+    }
+}
+
+struct BusListView_Previews: PreviewProvider {
+    static var previews: some View {
+        BusListView()
+    }
+}
---
+++ inu bus/FeedView.swift
@@ -0,0 +1,22 @@
+//
+//  FeedView.swift
+//  inu bus
+//
+//  Created by 송준영 on 2022/04/28.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var content: String = "test"
+    
+    var body: some View {
+        Text("아무말")
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
